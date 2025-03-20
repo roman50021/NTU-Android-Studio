24177bc83278acbb4593f6fc53a3895533298077
Roman <fedkoroma2@gmail.com>
2025-03-20 16:54:34 +0200

    LR 1

---
+++ app/src/main/java/com/example/lr1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lr1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textInfo;
+    private Button buttonShowInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textInfo = findViewById(R.id.textInfo);
+        buttonShowInfo = findViewById(R.id.buttonShowInfo);
+
+        buttonShowInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textInfo.setText(
+                        "Прізвище: Федько\n" +
+                                "Ім'я: Роман\n" +
+                                "По батькові: Вадимович\n" +
+                                "Спеціальність: Програмна інженерія\n" +
+                                "Курс: 4, Група: ПІ-4-1\n" +
+                                "Мета: Отримати глибокі знання з мобільної розробки"
+                );
+            }
+        });
+    }
+}
